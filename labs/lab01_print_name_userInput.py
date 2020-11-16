e1e60f9af9a81fedb7d720d8e687fb5030bc43b6
johnwchen <johnwchen@gmail.com>
2020-11-16 18:07:44 +0000

    cleanup

---
+++ labs/lab01_print_name_userInput.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+
+user_name = input("Please enter your name. ")
+day_of_week = input("Please enter day of the week. ")
+
+print(f"Hello, {user_name}! Happy {day_of_week}!")
