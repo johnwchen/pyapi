900b8045d77fb2482b96fb645643d37c24602a77
John Chen <john@ool-2f1088cd.dyn.optonline.net>
2020-11-17 10:39:25 -0500

    add

---
+++ labs/slicedemo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+challenge= ["science", "turbo", ["goggles", "eyes"], "nothing"]
+trial= ["science", "turbo", {"eyes": "goggles", "goggles": "eyes"}, "nothing"]
+nightmare= [{"slappy": "a", "text": "b", "kumquat": "goggles", "user":{"awesome": "c", "name": {"first": "eyes", "last": "toes"}},"banana": 15, "d": "nothing"}]
+
+def chal(challenge):
+    gog = challenge[2][0]
+    eye = challenge[2][1]
+    noth = challenge[3]
+    print(f"My {eye}! The {gog} do {noth}")
+
+def tri(trail):
+    eye = trail[2]['goggles']
+    gog = trail[2]['eyes']
+    noth = trail[3]
+    print(f"My {eye}! The {gog} do {noth}")
+
+    #print(eye)
+
+
+if __name__ == "__main__":
+    chal(challenge)
+    tri(trial)
