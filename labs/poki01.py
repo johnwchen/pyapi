ead45e2e681af57c979fa210761b0ed270b004ed
John Chen <john@ool-2f1088cd.dyn.optonline.net>
2020-11-18 10:37:57 -0500

    poki

---
+++ labs/poki01.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import requests
+import pprint
+
+def main():
+    pokeapi = requests.get("https://pokeapi.co/api/v2/pokemon/1").json()
+    print(f"Name of my pokimon: {pokeapi['name']}")
+    #pprint.pprint(pokeapi['sprites'])
+    print(f"URL: {pokeapi['sprites']['other']['official-artwork']['front_default']}")
+    #print("")
+    game_indices = pokeapi['game_indices']
+    #pprint.pprint(game_indices)
+    indice_count = 0
+    for indice in game_indices:
+        #print(indice)
+        indice_count +=1
+    print(f"Total Number of game_indices for {pokeapi['name']} is \"{indice_count}\"")
+    print("")
+    poki_moves = pokeapi['moves']
+    #print(poki_moves)
+    move_count = 0
+    for mv in poki_moves:
+        move_count +=1
+    print(move_count)
+
+        
+
+if __name__ == "__main__":
+    main()
